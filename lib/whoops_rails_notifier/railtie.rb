37ab9603bc4c1bd0d06425b15b9177f0859c4593
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-06-10 05:46:22 -0400

    basic setup

--- lib/whoops_rails_notifier/railtie.rb
+++ lib/whoops_rails_notifier/railtie.rb
@@ -2,6 +2,10 @@ module WhoopsRailsNotifier
   class Railtie < Rails::Railtie
     # config.app_middleware.insert_after "ActionDispatch::Callbacks"
     initializer "whoops_rails_notifier.extend_action_controller_base" do
+      config = YAML.load(File.join(Rails.root, "config", "whoops.yml"))[Rails.env]
+      WhoopsNotifier.config.set(config)
+      WhoopsNotifier.config.logger = Rails.logger
+      
       ActionController::Base.send(:include, WhoopsRailsNotifier::ActionControllerCatcher)
       ActionController::Base.send(:include, WhoopsRailsNotifier::ControllerMethods)
     end
--- whoops_rails_notifier.gemspec
+++ whoops_rails_notifier.gemspec
@@ -10,7 +10,6 @@ Gem::Specification.new do |s|
   s.add_dependency("whoops_notifier")
   s.add_dependency('rails', '~>3')
 
-  s.add_development_dependency('rspec', '2.1.0')
   s.add_development_dependency('rspec-rails')
   s.add_development_dependency('ruby-debug')
   s.add_development_dependency("capybara", "0.4.0")
